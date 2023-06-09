3b3c9c1af11f0d30e75ec8d9aba149df70c3133c
DemetrioGonzalez <demetrio2014gonzalez@hotmail.com>
2023-06-09 08:02:50 -0500

    parte 2

---
+++ CRS/Controllers/SegUsuariosController.cs
@@ -0,0 +1,62 @@
+﻿using CRS.Models.Respuesta;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CRS.Controllers
+{
+    public class SegUsuariosController : Controller
+    {
+        Respuesta respuesta = new Respuesta();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //[Route("api/login/Sesion")]
+        //public async Task<JsonResult> Sesion(string usuario, string contraseña)
+        //{
+            
+        //    //try
+        //    //{
+                
+
+        //    //    aperturaCaja.Id_empresa = idEmpresa;
+        //    //    aperturaCaja.Ususario_nombre = usuarioNombre;
+        //    //    aperturaCaja.Ususario_cedula = usuario;
+        //    //    aperturaCaja.Id_emision = 20;///adminitracion
+        //    //    respuesta = await _aperturaCaja.GrabarAperturaCaja(aperturaCaja);
+
+        //    //    if (_resultado != null)
+        //    //    {
+        //    //        return Json(new
+        //    //        {
+        //    //            mensaje_control = respuesta.mensaje_control,
+        //    //            error = respuesta.error,
+        //    //            respuesta_1 = respuesta.respuesta_1
+
+        //    //        });
+        //    //    }
+        //    //    else
+        //    //    {
+        //    //        return Json(new
+        //    //        {
+        //    //            mensaje_control = "Erro al Grabar Apertura Caja",
+        //    //            error = TipoError.Error,
+        //    //        });
+        //    //    }
+        //    //}
+        //    //catch (Exception ex)
+        //    //{
+                
+
+        //    //    return Json(new
+        //    //    {
+        //    //        mensaje_control = ex.Message,
+        //    //        error = TipoError.Error,
+        //    //    });
+        //    //}
+        //}
+
+    }
+}
---
+++ CRS/Models/Respuesta/Respuesta.cs
@@ -0,0 +1,9 @@
+﻿namespace CRS.Models.Respuesta
+{
+    public class Respuesta
+    {
+        public int codigo { get;set; }
+        public string respuesta { get;set; }
+        public object datos { get;set; }
+    }
+}
